a3ade4a8189b50e444258f71ac49ad69f9a23dc0
Pavel Belevich <pbelevich@fb.com>
2019-12-18 14:36:24 -0800

    at::Generator refactoring: extracting MT19937CPUGenerator from CPUGenerator
    ghstack-source-id: f5bcfb28071cdc39649b217f0a49682a89dc7b12
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/31440

--- aten/src/ATen/CPUGenerator.h
+++ aten/src/ATen/CPUGenerator.h
@@ -1,8 +1,6 @@
 #pragma once
 
 #include <ATen/core/Generator.h>
-#include <ATen/core/MT19937RNGEngine.h>
-#include <ATen/core/PhiloxRNGEngine.h>
 #include <c10/util/Optional.h>
 
 namespace at {
@@ -10,26 +8,30 @@ namespace at {
 struct CAFFE2_API CPUGenerator : public Generator {
   // Constructors
   CPUGenerator(uint64_t seed_in = default_rng_seed_val);
-  ~CPUGenerator() = default;
+  virtual ~CPUGenerator() = default;
 
   // CPUGenerator methods
   std::shared_ptr<CPUGenerator> clone() const;
-  void set_current_seed(uint64_t seed) override;
-  uint64_t current_seed() const override;
-  uint64_t seed() override;
+  virtual void set_current_seed(uint64_t seed) = 0;
+  virtual uint64_t current_seed() const = 0;
+  virtual uint64_t seed() = 0;
   static DeviceType device_type();
-  uint32_t random();
-  uint64_t random64();
+  /**
+   * Gets a random 32 bit unsigned integer from the engine
+   *
+   * See Note [Acquire lock when using random generators]
+   */
+  virtual uint32_t random() = 0;
+  virtual uint64_t random64();
   c10::optional<float> next_float_normal_sample();
   c10::optional<double> next_double_normal_sample();
   void set_next_float_normal_sample(c10::optional<float> randn);
   void set_next_double_normal_sample(c10::optional<double> randn);
-  at::mt19937 engine();
-  void set_engine(at::mt19937 engine);
+  virtual void getRNGState(void* target) = 0;
+  virtual void setRNGState(void* target) = 0;
 
-private:
-  CPUGenerator* clone_impl() const override;
-  at::mt19937 engine_;
+protected:
+  // CPUGenerator* clone_impl() const override;
   c10::optional<float> next_float_normal_sample_;
   c10::optional<double> next_double_normal_sample_;
 };
